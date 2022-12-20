9f8f5fbb85b6b8f974df410eaa977c302ae48763
arfa <arfa.madi@tapsi.cab>
2022-12-20 12:40:48 +0330

    add result page and navigate to it when the time is finished

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import "./index.css";
 import Game from "./pages/Game";
+import Result from "./pages/Result";
 
 const router = createBrowserRouter([
 	{
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
 	{
 		path: "/game",
 		element: <Game />,
+	},
+	{
+		path: "/result",
+		element: <Result />,
 	},
 ]);
 
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+("react-router-dom");
 import useCountdownTimer from "../features/game/timer/useTimer";
 
 export default function Game() {
 	const timer = useCountdownTimer(3, showResult);
+	const navigate = useNavigate();
 
-	function showResult() {}
+	function showResult() {
+		navigate("/result");
+	}
 
 	return <div>{timer}</div>;
 }
