1ee163aa91654f9b31d9ba526affe14e6aa23aa0
arfa <arfa.madi@tapsi.cab>
2022-12-24 16:14:45 +0330

    fetch and show images

--- src/features/game/field/Field.jsx
+++ src/features/game/field/Field.jsx
@@ -1,35 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { getDogPicUrl, getCatPicUrl } from "./getPicUrl";
+import styles from "./Field.module.css";
 
 export default function Field() {
 	const dogAPIBaseUrl = "https://dog.ceo/api/breeds/image/random";
 	const catAPIBaseUrl = "https://api.thecatapi.com/v1/images/search";
-	const [dogPicUrls, setDogPicUrls] = useState([]);
-	const [catPicUrls, setCatPicUrls] = useState([]);
-	const [picsList, setPicsList] = useState([]);
+	const [picsList, setPicsList] = useState({ urls: [], isMixed: false });
+	const [isAllPicsLoaded, setIsAllPicsLoaded] = useState(false);
 
-	function picMixer() {
-		const dogsPic = [...dogPicUrls];
-		const catsPic = [...catPicUrls];
-		for (let i = 0; i < 9; i++) {
-			const isCatPicTurn = !!Math.round(Math.random());
-			const catPic = isCatPicTurn && !!catsPic.length && catsPic.shift();
-			const dogPic = !isCatPicTurn && !!dogsPic.length && dogsPic.shift();
-			console.log(isCatPicTurn);
-			console.log("catPic", catPic);
-			console.log("dogPic", dogPic);
-			//setPicsList((prevState) => [...prevState,]);
+	function picMixer(arrayState, setter) {
+		const pics = [...arrayState];
+		for (let i = pics.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[pics[i], pics[j]] = [pics[j], pics[i]];
 		}
+		setter({ urls: [...pics], isMixed: true });
 	}
 
+	function onLoad() {
+		let loadedPicNumbers = 0;
+		function loadedPicCounter() {
+			loadedPicNumbers += 1;
+			if (loadedPicNumbers === 9) setIsAllPicsLoaded(true);
+		}
+		return loadedPicCounter;
+	}
+
+	const loadedPicCounter = onLoad();
+
 	useEffect(() => {
-		getDogPicUrl(dogAPIBaseUrl, 7, setDogPicUrls);
-		getCatPicUrl(catAPIBaseUrl, 2, setCatPicUrls);
+		getDogPicUrl(dogAPIBaseUrl, 7, setPicsList);
+		getCatPicUrl(catAPIBaseUrl, 2, setPicsList);
 	}, []);
+
+	useEffect(() => {
+		if (picsList.urls.length === 9 && !picsList.isMixed)
+			picMixer(picsList.urls, setPicsList);
+	}, [picsList]);
 
 	return (
 		<div>
-			<button onClick={picMixer}>mix</button>
+			{picsList.isMixed &&
+				picsList.urls.map((url) => (
+					<img
+						className={
+							isAllPicsLoaded ? styles["img--visible"] : styles["img--hidden"]
+						}
+						src={url}
+						onLoad={loadedPicCounter}
+					/>
+				))}
 		</div>
 	);
 }
--- src/features/game/field/getPicUrl.js
+++ src/features/game/field/getPicUrl.js
@@ -1,16 +1,22 @@
 import axios from "axios";
 
 export function getDogPicUrl(baseUrl, number, setUrls) {
-	for (let i = 0; i <= number; i++) {
-		axios
-			.get(baseUrl)
-			.then((res) => setUrls((prevState) => [...prevState, res.data.message]));
+	for (let i = 0; i < number; i++) {
+		axios.get(baseUrl).then((res) =>
+			setUrls((prevState) => ({
+				urls: [...prevState.urls, res.data.message],
+				isMixed: false,
+			}))
+		);
 	}
 }
 export function getCatPicUrl(baseUrl, number, setUrls) {
-	for (let i = 0; i <= number; i++) {
-		axios
-			.get(baseUrl)
-			.then((res) => setUrls((prevState) => [...prevState, res.data[0].url]));
+	for (let i = 0; i < number; i++) {
+		axios.get(baseUrl).then((res) =>
+			setUrls((prevState) => ({
+				urls: [...prevState.urls, res.data[0].url],
+				isMixed: false,
+			}))
+		);
 	}
 }
