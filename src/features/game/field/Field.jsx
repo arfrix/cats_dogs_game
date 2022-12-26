e608eaadef7c1f1ab59b5188055e1bd45a3cb793
arfa <arfa.madi@tapsi.cab>
2022-12-26 12:45:27 +0330

    load new pics after a pic selcted

--- src/features/game/field/Field.jsx
+++ src/features/game/field/Field.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { getDogPicUrl, getCatPicUrl } from "./getPicUrl";
 import { useDispatch, useSelector } from "react-redux";
-import { decreaseScore, increaseScore } from "../gameSlice";
+import { decreaseScore, increaseScore, setPicLoading } from "../gameSlice";
 import styles from "./Field.module.css";
 
 export default function Field() {
 	const dogAPIBaseUrl = "https://dog.ceo/api/breeds/image/random";
 	const catAPIBaseUrl = "https://api.thecatapi.com/v1/images/search";
 	const [picsList, setPicsList] = useState({ urls: [], isMixed: false });
+	const [loadedPicNumbers, setLoadedPicNumbers] = useState(0);
 	const [isAllPicsLoaded, setIsAllPicsLoaded] = useState(false);
 	const dispatch = useDispatch();
 	const gameTarget = useSelector((state) => state.game.gameTarget);
+	const loading = useSelector((state) => state.game.isPicLoading);
 
 	function picMixer(arrayState, setter) {
 		const pics = [...arrayState];
@@ -27,42 +29,57 @@ export default function Field() {
 		} else {
 			dispatch(decreaseScore());
 		}
+		dispatch(setPicLoading(true));
+		setIsAllPicsLoaded(false);
+		setLoadedPicNumbers(0);
+		getPics();
 	}
 
 	function onLoad() {
-		let loadedPicNumbers = 0;
-		function loadedPicCounter() {
-			loadedPicNumbers += 1;
-			if (loadedPicNumbers === 9) setIsAllPicsLoaded(true);
-		}
-		return loadedPicCounter;
+		setLoadedPicNumbers((prevState) => prevState + 1);
 	}
 
-	const loadedPicCounter = onLoad();
-
-	useEffect(() => {
+	function getPics() {
+		dispatch(setPicLoading(true));
+		setPicsList({ urls: [], isMixed: false });
 		getDogPicUrl(dogAPIBaseUrl, 7, setPicsList);
 		getCatPicUrl(catAPIBaseUrl, 2, setPicsList);
+	}
+
+	useEffect(() => {
+		getPics();
 	}, []);
 
 	useEffect(() => {
 		if (picsList.urls.length === 9 && !picsList.isMixed)
 			picMixer(picsList.urls, setPicsList);
+
+		// is there a proper place to dispatch setPicLoading() ? because put it at the end of picMixer() can't guarantee that execute it after setter !
+		if (picsList.urls.length === 9 && picsList.isMixed)
+			dispatch(setPicLoading(false));
 	}, [picsList]);
+
+	useEffect(() => {
+		if (loadedPicNumbers === 9) setIsAllPicsLoaded(true);
+	}, [loadedPicNumbers]);
 
 	return (
 		<div>
-			{picsList.isMixed &&
+			{loading ? (
+				<div> loading </div>
+			) : (
+				picsList.isMixed &&
 				picsList.urls.map((pic) => (
 					<img
 						className={
 							isAllPicsLoaded ? styles["img--visible"] : styles["img--hidden"]
 						}
 						src={pic.url}
-						onLoad={loadedPicCounter}
+						onLoad={onLoad}
 						onClick={() => onClick(pic.type)}
 					/>
-				))}
+				))
+			)}
 		</div>
 	);
 }
--- src/features/game/gameSlice.js
+++ src/features/game/gameSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
 	score: 0,
 	gameTarget: "",
+	isPicLoading: true,
 };
 
 export const gameSlice = createSlice({
@@ -13,15 +14,18 @@ export const gameSlice = createSlice({
 			state.score += 1;
 		},
 		decreaseScore: (state) => {
-			state.score -= 1;
+			state.score -= 0.5;
 		},
 		setGameTarget: (state, action) => {
 			state.gameTarget = action.payload;
 		},
+		setPicLoading: (state, action) => {
+			state.isPicLoading = action.payload;
+		},
 	},
 });
 
-export const { increaseScore, decreaseScore, setGameTarget } =
+export const { increaseScore, decreaseScore, setGameTarget, setPicLoading } =
 	gameSlice.actions;
 
 export default gameSlice.reducer;
